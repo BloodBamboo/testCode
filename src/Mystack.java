bb22fa019c95055aedcee630dcf7e0bf692e1cac
yangx <yangx@edu-edu.com.cn>
2017-12-26 14:25:24 +0800

    添加栈结构实际应用练习

--- src/Mystack.java
+++ src/Mystack.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 
 public class Mystack<T> {
 
@@ -48,14 +50,57 @@ public class Mystack<T> {
 		}
     }
     
+    
+    /**
+     * 给定一个字符串，其中只包含()，[]，{},判定字符串中的括号匹配是否合法。
+
+比如 “()”，“()[]{}”是合法的，“(}”，“([)]”是非法的。
+     * 用一个Stack来存储括号， 主要考察对栈数据结构的操作。
+
+算法的时间复杂度是O(n)，空间复杂度也是O(n)。
+
+遍历传入的String s，如果遇到左括号就入栈；如果遇到右括号，检查栈如果为空，证明不能匹配，如果栈不空，pop出栈顶的元素，看是否与当前的右括号匹配。
+
+如果匹配，继续向下进行新一轮的循环，如果不匹配，返回false.
+     * @param s
+     * @return
+     */
+    public static boolean isValid(String s) {
+    	char[] chars = s.toCharArray();
+    	Mystack<Character> stack = new Mystack<Character>();
+    	for (int i = 0; i < chars.length; i++) {
+    		char t = chars[i];
+    		if (t == '(' || t == '{' || t == '[') {
+    			stack.push(t);
+    		} else {
+    			if (t == ')') {
+					if (stack.isEmpty() || stack.pop() != '(') {
+						return false;
+					}
+    			} else if (t == '}') {
+					if (stack.isEmpty() || stack.pop() != '{') {
+						return false;
+					}
+    			}else if (t == ']') {
+					if (stack.isEmpty() || stack.pop() != '[') {
+						return false;
+					}
+    			}
+    		}
+    	}
+    	
+    	return stack.isEmpty();
+    }
+    
     public static void main(String[] args) {
-    	Mystack<String> stack = new Mystack<String>();
-		for (int i = 0; i < 5; i++) {
-			stack.push(i + "");
-		}
-	
-		while (!stack.isEmpty()) {
-			System.out.println(stack.pop());
-		}
+//    	Mystack<String> stack = new Mystack<String>();
+//		for (int i = 0; i < 5; i++) {
+//			stack.push(i + "");
+//		}
+//	
+//		while (!stack.isEmpty()) {
+//			System.out.println(stack.pop());
+//		}
+    	System.out.print(isValid("{([])}"));
 	}
 }
--- src/tt.java
+++ src/tt.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Stack;
 
 public class tt {
 
@@ -18,46 +19,20 @@ public class tt {
       TreeNode(int x) { val = x; }
   }
 
-
-
-    public static int minDepth(TreeNode root) {
-        if (root == null) {
-            return 0;
-        }
-        return pre(root, 0);
-    }
-
-    public static int pre(TreeNode root, int l) {
-        if (root == null) {
-            return l;
-        }
-        l++;
-        if (root.left == null && root.right == null) {
-            return l;
-        }
-        int left = pre(root.left, l);
-        int right = pre(root.right, l);
-
-        if (root.left != null && root.right != null) {
-            return left < right ? left : right;
-        } else if (root.left != null) {
-            return left;
-        } else {
-            return right;
-        }
-    }
-
     /**
      * @param args
      */
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(1);
+        TreeNode root = new TreeNode(4);
         root.left = new TreeNode(2);
-//        root.right = new TreeNode(3);
-//        root.left.left= new TreeNode(4);
-//        root.left.right= new TreeNode(5);
-        print(minDepth(root));
+        root.right = new TreeNode(7);
+        root.left.left= new TreeNode(1);
+        root.left.right= new TreeNode(3);
+        root.right.left= new TreeNode(6);
+        root.right.right= new TreeNode(9);
+        
+        
     }
 
 
